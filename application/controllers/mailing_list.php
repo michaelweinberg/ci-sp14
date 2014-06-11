f8830d077567337abc44666c842207ea98716aa7
michael <weinberg4@gmail.com>
2014-06-11 09:44:48 -0700

    Adds insert id to Add page and redirects to view of mailing list

--- application/controllers/mailing_list.php
+++ application/controllers/mailing_list.php
@@ -48,9 +48,54 @@
 			
 		}
 		
-		public function insert(){
-			//will insert the data entered via add
+		public function insert(){//will insert data via add
+			$this->load->model('Mailing_list_model');
+			$this->load->library('form_validation');
+			$this->form_validation->set_rules('email','Email','trim|required|valid_email');
+                        $this->form_validation->set_rules('first_name','First Name','trim|required');
+                        $this->form_validation->set_rules('last_name','Last Name','trim|required');
+                        $this->form_validation->set_rules('address','Address','trim|required');
+                        $this->form_validation->set_rules('zip_postal','Zip Code','trim|required');
+                        $this->form_validation->set_rules('username','Username','trim|required');
+                        $this->form_validation->set_rules('password','Password','trim|required');
+			//must have at least one validation rule to insert			
+
 			
+			if($this->form_validation->run() == FALSE){
+				//view data on failure goes here
+				//form to add a new record
+			$this->load->helper('form');
+			$data['title'] = "Adding a record";
+			$data['style'] = "cerulean.css";
+			$data['banner'] = "Data Entry Error";
+			$data['copyright'] = "copyright goes here!";
+			$data['base_url'] = base_url();
+			$this->load->view('header',$data);
+			$this->load->view('add_mailing_list',$data);
+			$this->load->view('footer',$data);
+				
+
+			}else{//insert data
+				$post = array(
+					'first_name' => $this->input->post('first_name'),
+					'last_name' => $this->input->post('last_name'),
+					'email' => $this->input->post('email'),
+					'address' => $this->input->post('address'),
+					'zip_postal' => $this->input->post('zip_postal'),
+					'username' => $this->input->post('username'),
+					'password' => $this->input->post('password'),
+					'bio' => $this->input->post('bio'),
+					'interests' => $this->input->post('interests'),
+					'num_tours1' => $this->input->post('num_tours1'),
+					'num_tours2' => $this->input->post('num_tours2'),
+					'num_tours3' => $this->input->post('num_tours3')
+				);
+				
+				$id = $this->Mailing_list_model->insert($post);
+				
+				redirect('/mailing_list/view/' . $id);
+				//echo "data inserted";
+			}
 			
 		}
 	}
--- application/models/mailing_list_model.php
+++ application/models/mailing_list_model.php
@@ -12,13 +12,14 @@
 		
 		public function get_id($id){
 			//will show all data in table name mail_list
-			$this->db->select('userid,first_name,last_name,email,address,state_code,zip_postal,bio');
+			// $this->db->select('userid,first_name,last_name,email,address,state_code,zip_postal,bio');
 			$this->db->where('userid', $id);
 			return $this->db->get('mailing_list');
 		}//end get_id
 		
 		public function insert(){
 			$this->db->insert('mailing_list', $row);
+			return $this->db->insert_id();
 		}//end insert
     }
     
